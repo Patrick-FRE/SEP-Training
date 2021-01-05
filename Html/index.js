db071089e914e58b2b3c3286fe9ae9b7f8f7199d
show3567 <ydong011@fiu.edu>
2021-01-05 15:36:09 -0500

    commit day1

---
+++ Html/index.js
@@ -0,0 +1,19 @@
+console.log('hello')
+
+// function getElementsByClassName(className) {
+//     const nodeList = [];
+//     function get(node) {
+
+//         if(node.classList === className) {
+//             nodeList.push(node.classList[count]);
+//         }
+//         for (let i = 0; i < node.childNodes.length; i++) {
+//             get(node.childNodes[i]);
+//         }
+//     }
+
+//     get(document.body);
+//     return nodeList;
+// }
+
+// getElementsByClassName('test');
