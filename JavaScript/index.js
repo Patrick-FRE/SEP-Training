e3c5f01a1a1ab3d68e8355c5b8e442f699b6b957
show3567 <ydong011@fiu.edu>
2021-01-15 14:30:57 -0500

    finished todo

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -630,87 +630,87 @@
 //     .catch(err => console.log(err));
 
 // MyPromise
-class MyPromise {
-    promiseState = 'pendding';
-    thenCallBackQueue = [];
-    catchCallBackQueue = [];
-    currentData;
-    currentError;
+// class MyPromise {
+//     promiseState = 'pendding';
+//     thenCallBackQueue = [];
+//     catchCallBackQueue = [];
+//     currentData;
+//     currentError;
 
-    constructor(exe) {
-        exe(this.resolve, this.reject.bind(this));
-    }
-    resolve = (data) => {
-        if (this.promiseState === 'failed') return;
-        this.promiseState = 'fulfilled';
-        setTimeout(() => {
-            this.currentData = data;
+//     constructor(exe) {
+//         exe(this.resolve, this.reject.bind(this));
+//     }
+//     resolve = (data) => {
+//         if (this.promiseState === 'failed') return;
+//         this.promiseState = 'fulfilled';
+//         setTimeout(() => {
+//             this.currentData = data;
 
-            while (this.thenCallBackQueue.length) {
-                const cb = this.thenCallBackQueue.shift();
-                if (this.currentData instanceof MyPromise) {
-                    // this.promiseState = 'pendding';
-                    this.currentData.then(dataFromRes => {
-                        this.currentData = cb(dataFromRes)
-                    });
-                    this.currentData.catch(error => {
-                        this.currentError = error;
-                    });
-                } else {
-                    this.currentData = cb(this.currentData);
-                }
-            }
-        }, 0);
-    }
-    reject(error) {
-        if (this.promiseState === 'fulfilled') return;
-        this.promiseState = 'failed';
-        setTimeout(() => {
-            this.currentError = error;
-            if (this.catchCallBackQueue.length) {
-                const cb = this.catchCallBackQueue.shift();
-                cb(this.currentError);
-            }
-        }, 0);
-    }
-    then(thenCallBack) {
-        this.thenCallBackQueue.push(thenCallBack);
-        return this;
-    }
-    catch(catchCallBack) {
-        this.catchCallBackQueue.push(catchCallBack);
-    }
+//             while (this.thenCallBackQueue.length) {
+//                 const cb = this.thenCallBackQueue.shift();
+//                 if (this.currentData instanceof MyPromise) {
+//                     // this.promiseState = 'pendding';
+//                     this.currentData.then(dataFromRes => {
+//                         this.currentData = cb(dataFromRes)
+//                     });
+//                     this.currentData.catch(error => {
+//                         this.currentError = error;
+//                     });
+//                 } else {
+//                     this.currentData = cb(this.currentData);
+//                 }
+//             }
+//         }, 0);
+//     }
+//     reject(error) {
+//         if (this.promiseState === 'fulfilled') return;
+//         this.promiseState = 'failed';
+//         setTimeout(() => {
+//             this.currentError = error;
+//             if (this.catchCallBackQueue.length) {
+//                 const cb = this.catchCallBackQueue.shift();
+//                 cb(this.currentError);
+//             }
+//         }, 0);
+//     }
+//     then(thenCallBack) {
+//         this.thenCallBackQueue.push(thenCallBack);
+//         return this;
+//     }
+//     catch(catchCallBack) {
+//         this.catchCallBackQueue.push(catchCallBack);
+//     }
 
-    static all(array) {
-        let counter = 0;
-        const arrLength = array.length;
-        const resolveData = new Array(arrLength);
+//     static all(array) {
+//         let counter = 0;
+//         const arrLength = array.length;
+//         const resolveData = new Array(arrLength);
 
-        return new MyPromise((res, rej) => {
-            array.forEach((ele, i) => {
-                ele.then(data => {
-                    counter++;
-                    resolveData[i] = data;
-                    if (arrLength === counter) res(resolveData);
-                })
-            });
-        });
-    }
-}
+//         return new MyPromise((res, rej) => {
+//             array.forEach((ele, i) => {
+//                 ele.then(data => {
+//                     counter++;
+//                     resolveData[i] = data;
+//                     if (arrLength === counter) res(resolveData);
+//                 })
+//             });
+//         });
+//     }
+// }
 
-const p = new Promise((resolve, reject) => {
-        resolve('hello');
-        // reject(new Error('fail'));
-})
-    .then(data => {
-        console.log('this is data: ', data);
-        return new Promise((res, rej) => {
-            res('Dio');
-            rej('Jojo');
-        });
-    })
-    .then(data2 => console.log('this is data2: ', data2))
-    .catch(err => console.log(err));
+// const p = new Promise((resolve, reject) => {
+//         resolve('hello');
+//         // reject(new Error('fail'));
+// })
+//     .then(data => {
+//         console.log('this is data: ', data);
+//         return new Promise((res, rej) => {
+//             res('Dio');
+//             rej('Jojo');
+//         });
+//     })
+//     .then(data2 => console.log('this is data2: ', data2))
+//     .catch(err => console.log(err));
 
 
 // shift, unshift, pop, push
@@ -785,3 +785,9 @@ const p = new Promise((resolve, reject) => {
 //     });
 // }
 
+function foo() {
+    console.log(a);
+    let a = 5;
+}
+foo();
+console.log(typeof undefined)
--- todolist/index.js
+++ todolist/index.js
@@ -10,15 +10,28 @@ const todoAPI = (() => {
         method: 'DELETE',
     });
 
+    const addTodo = (newTodo) => {
+        return fetch([baseUrl, todoPath].join('/'), {
+            method: 'POST',
+            body: JSON.stringify(newTodo),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json());
+    }
+
     return {
         getAllTodos,
-        delefeTodo
+        delefeTodo,
+        addTodo
     }
 })();
 
 const View = (() => {
     const domString = {
-        todolist: 'todolist-content'
+        todolist: 'todolist-content',
+        todoinput: 'todolist__input'
     }
     const render = (element, htmlTemplate) => {
         element.innerHTML = htmlTemplate;
@@ -42,80 +55,93 @@ const View = (() => {
     }
 })();
 
-const Model = ((api) => {
+const Model = ((api, view) => {
     class Todo {
-        constructor(userId, id, title, completed) {
+        constructor(userId, title, completed) {
             this.userId = userId;
-            this.id = id;
             this.title = title;
             this.completed = completed;
         }
     }
-
-    const fetchTodos = api.getAllTodos;
-    const deleteTodo = api.delefeTodo;
-
-    return {
-        fetchTodos,
-        deleteTodo,
-        Todo
-    }
-})(todoAPI);
-
-const AppController = ((view, model) => {
     class State {
         #todolist = [];
+        #todoinput = '';
+
+        get todoinput() {
+            return this.#todoinput;
+        }
+        set todoinput(newinput) {
+            this.#todoinput = newinput;
+            const todoInputEle = document.querySelector('.' + view.domString.todoinput);
+            todoInputEle.value = this.#todoinput;
+        }
 
         get todolist() {
             return this.#todolist;
         }
         set todolist(newlist) {
             this.#todolist = newlist;
+            
             const element = document.getElementById(view.domString.todolist);
             const tmp = view.inittodoListTmp(this.#todolist);
             view.render(element, tmp);
-
-            // const btnRemove = document.querySelectorAll('.btn-remove');
-            // btnRemove.forEach(ele => {
-            //     ele.addEventListener('click', () => {
-            //         state.todolist = state.todolist.filter(todo => {
-            //             return +ele.id !== +todo.id;
-            //         });
-            //     });
-            // });
         }
     }
-    const state = new State();
+
+    const fetchTodos = api.getAllTodos;
+    const deleteTodo = api.delefeTodo;
+    const addTodo = api.addTodo;
+
+    return {
+        fetchTodos,
+        deleteTodo,
+        addTodo,
+        State,
+        Todo
+    }
+})(todoAPI, View);
+
+const AppController = ((view, model) => {
+
+    const state = new model.State();
+
+    const addListenerOnInput = () => {
+        const todoInputEle = document.querySelector('.' + view.domString.todoinput);
+
+        todoInputEle.addEventListener('keyup', (event) => {
+            if (event.key === 'Enter') {
+                state.todoinput = event.target.value;
+                console.log(state.todoinput);
+                
+                // const randomId = Math.random() * 1000000 ** 2;
+                const newTodo = new model.Todo(1, state.todoinput, false);
+                
+                model.addTodo(newTodo).then(data => {
+                    state.todolist = [data, ...state.todolist];
+                    console.log(data);
+                });
+                state.todoinput = '';
+                // event.target.value = '';
+            }
+        });
+    }
+
+    const addListenerOnRemove = () => {
+        const todolistContent = document.querySelector('#' + view.domString.todolist);
+        todolistContent.addEventListener('click', (event) => {
+            if (event.target.className === 'btn-remove') {
+                state.todolist = state.todolist.filter(todo => {
+                    return +event.target.id !== +todo.id;
+                });
+            }
+        });
+    }
 
     const initTodos = () => {
         model.fetchTodos().then(data => {
             state.todolist = data;
-
-            // const element = document.getElementById(view.domString.todolist);
-            // const tmp = view.inittodoListTmp(data);
-            // view.render(element, tmp);
-
-            const todolistContent = document.querySelector('#' + view.domString.todolist);
-            // console.log(todolistContent)
-            todolistContent.addEventListener('click', (event) => {
-                if (event.target.className === 'btn-remove') {
-                    // console.log(event.target);
-                    state.todolist = state.todolist.filter(todo => {
-                        return +event.target.id !== +todo.id;
-                    });
-                }
-            })
-
-            // const btnRemove = document.querySelectorAll('.btn-remove');
-            // btnRemove.forEach(ele => {
-            //     ele.addEventListener('click', () => {
-            //         // console.log(event.target.id);
-            //         state.todolist = state.todolist.filter(todo => {
-            //             return +ele.id !== +todo.id;
-            //         });
-            //         console.log(state.todolist.length);
-            //     });
-            // })
+            addListenerOnRemove();
+            addListenerOnInput();
         });
     }
     const init = () => {
