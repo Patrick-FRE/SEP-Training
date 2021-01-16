869f3f5e3fcca179ed5198de75b94a03668bb35b
Danny Lin <dannyjabez@gmail.com>
2021-01-16 15:23:32 -0500

    making the search  page

--- Assignment_ItunesMusic_0115/script.js
+++ Assignment_ItunesMusic_0115/script.js
@@ -6,34 +6,37 @@ button.addEventListener('click', (e) => {
      getDataFromItunes()
 })
 
+
+input.addEventListener('input', (e) => {
+    getDataFromItunes()
+})
+// for test
+// (getDataFromItunes())();
+
+//shakira
 function getDataFromItunes(){
     let input = document.querySelector("#search");
     let url = 'https://itunes.apple.com/search?term=' + input.value
 
+    // for test
+    // url = "https://itunes.apple.com/search?term=shakira"
+    
+    
     fetch(url)
     .then(data => data.json())  // data是回傳的資料, 轉成json格式
     .then(json => {
         console.log(json)
         let finalHTML = ''
-
         json.results.forEach(song => {
             finalHTML += 
             `
-                <div class="col s4 m4 l4">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="activator" src="${song.artworkUrl100}">
-                        </div>
-                        <div class="card-content">
-                            <span class="card-title">${song.trackCensoredName}</span>
-                            <p>${song.artistName}</p>
-                        </div>
-                        <div class="card-reveal">
-                            <span class="card-title">Card</span>
-                            <p>Here is some more information about this product</p>
-                        </div>
-                    </div>
-                </div>                
+            <div class="column">
+                <img src="${song.artworkUrl100}" alt="Avatar">
+                <div class="container">
+                    <h4>${song.trackCensoredName}</h4> 
+                    <p>${song.artistName}</p> 
+                </div>
+            </div>
             `
         })
         output.innerHTML = finalHTML
