f153aeaba32b823f454e2c5b55f05282e4e18713
Esther Qiu <qiu00057@umn.edu>
2021-01-17 14:39:07 -0600

    fixe card's height/width & bug fix card HTML

--- HW/week1/project/js/index.js
+++ HW/week1/project/js/index.js
@@ -18,8 +18,12 @@ var cardHTML =  `
     <div class="card__title">
         Example Blog Post
     </div>
-    <div class="card__date>Nov 12</div>
-    <div class="card__example">This card represents an example blog post</div>
+    <div class="card__date">
+        Nov 12
+    </div>
+    <div class="card__example">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupiLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
     <div class="card__continue">
         <a>Continue reading...</a>
     </div>
