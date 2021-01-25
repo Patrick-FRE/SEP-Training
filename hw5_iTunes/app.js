41b376386b895c8421f84b750d8f46c6c53eee41
LenaChu <a87192611@gmail.com>
2021-01-24 22:26:32 -0600

    WIP

--- hw5_iTunes/app.js
+++ hw5_iTunes/app.js
@@ -1,1 +1,41 @@
-const 
+const fetchAPI = ((model) => {
+  const baseURL = `https://itunes.apple.com/search?term=${model.artistName}&media=music&entity=album&attribute=artistTerm&limit=50`;
+  //artistName will from model
+
+  const getAllAlbums = () => fetch(baseURL).then((response) => response.json());
+
+  return {
+    getAllAlbums,
+  };
+})();
+
+const View = () => {
+  const domString = {
+    //get the album gallery container
+    albumGallery: "album-gallery",
+  };
+  const render = (element, htmlTemplate) => {
+    element.innerHTML = htmlTemplate;
+  };
+
+  const setGalleryTemplate = (albumArray) => {
+    let template = "";
+    albumArray.forEach((album) => {
+      template += `<div class="card-container">
+            <img class="album-cover" id="${album.collectionId}"
+                src="${album.artworkUrl100}">
+            <p>${album.collectionName}</p>
+        </div>`;
+    });
+
+    return {
+      domString,
+      render,
+      setGalleryTemplate,
+    };
+  };
+};
+
+const Model = () => {};
+
+const Controller = () => {};
