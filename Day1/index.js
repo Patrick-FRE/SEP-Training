a2ac95eda897e987c51b1a0b59493aca8a558cfa
Patrick-FRE <patrick.lin@antra.com>
2021-01-25 16:25:35 -0500

    update

--- Day1/index.js
+++ Day1/index.js
@@ -1,1 +1,34 @@
-console.log('hello');
+let component = {
+  state: {
+    counter: 0,
+  },
+};
+
+function setState(newState) {
+  if (typeof newState === 'function') {
+    setTimeout(() => {
+      component.state = newState(component.state);
+    }, 0);
+  } else {
+    setTimeout(() => {
+      component.state = newState;
+    }, 0);
+  }
+}
+
+setState((preState) => {
+  return {
+    counter: preState.counter + 1,
+  };
+});
+console.log(component.state); //
+setState((preState) => {
+  return {
+    counter: preState.counter + 1,
+  };
+});
+setTimeout(() => {
+  console.log(component.state); //
+}, 1000);
+// setState(() => {});
+// console.log(component.state);
--- React/React-basic/react-basic/src/components/Counter/Counter.js
+++ React/React-basic/react-basic/src/components/Counter/Counter.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+// var myVar = setInterval(myTimer, 1000);
+
+// var a = { state: { name: 'Patrick' } };
+// console.log(a.state);
+
+// let _intervalId = null;
+
+// function myStopFunction() {
+//   clearInterval(myVar);
+// }
+
 // REACT life cycle methods:
 // arrow function;
 class Counter extends React.Component {
@@ -14,20 +25,33 @@ class Counter extends React.Component {
     this.handleAdd = this.handleAdd.bind(this);
   }
   handleAdd() {
-    this.setState({
-      counter: this.state.counter + 1,
+    this.setState((preState) => {
+      return {
+        counter: preState.counter + 1,
+      };
+    });
+    this.setState((preState) => {
+      return {
+        counter: preState.counter + 1,
+      };
     });
   }
   // handleAdd = () => {
   //   console.log(this);
   // };
   componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        counter: this.state.counter + 1,
-      });
-      console.log('interval');
-    }, 1000);
+    // this._intervalId = setInterval(() => {
+    //   this.setState({
+    //     counter: this.state.counter + 1,
+    //   });
+    //   console.log('interval');
+    // }, 1000);
+  }
+
+  componentWillUnmount() {
+    //console.log('id' + this._intervalID);
+    //console.log(intervalId);
+    clearInterval(this._intervalId);
   }
 
   render() {
@@ -47,3 +71,16 @@ export default Counter;
 // const foo3 = foo.bind(this);
 
 // const foo2 = () => {};
+
+let state = {
+  counter: 0,
+};
+
+function setState(newState) {
+  state = newState;
+}
+
+setState({ counter: state + 1 });
+console.log(state); //
+setState({ counter: state + 1 });
+console.log(state);
