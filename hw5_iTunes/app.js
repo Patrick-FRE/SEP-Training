fc4ef4eab29c53e404b0bee3dfd01ec6cd1ebbf6
LenaChu <a87192611@gmail.com>
2021-01-26 22:17:15 -0600

    finish hw5

--- hw5_iTunes/app.js
+++ hw5_iTunes/app.js
@@ -1,18 +1,26 @@
-const fetchAPI = ((controller) => {
-  const baseURL = `https://itunes.apple.com/search?term=${controller.getInput.value}&media=music&entity=album&attribute=artistTerm&limit=50`;
-  //artistName will from Controller
-
-  const getAllAlbums = () => fetch(baseURL).then((response) => response.json());
+const fetchAPI = (() => {
+  const getAllAlbums = (name) => {
+    const artistName = name;
+    const baseURL = `https://itunes.apple.com/search?term=${artistName}&media=music&entity=album&attribute=artistTerm&limit=50`;
+    // console.log(baseURL);
+    return fetch(baseURL).then((response) => response.json());
+    // .then((repsonseJSON) => {
+    //   return repsonseJSON;
+    // });
+  };
 
   return {
     getAllAlbums,
   };
-})(Controller);
+})();
 
-const View = () => {
+const View = (() => {
   const domString = {
     //get the album gallery container
     albumGallery: "album-gallery",
+    searchResult: "search-result",
+    searchInput: "search-input",
+    // resultRow: "result-row"
   };
   const render = (element, htmlTemplate) => {
     element.innerHTML = htmlTemplate;
@@ -27,15 +35,91 @@ const View = () => {
             <p>${album.collectionName}</p>
         </div></a>`;
     });
-
-    return {
-      domString,
-      render,
-      setGalleryTemplate,
-    };
+    return template;
   };
-};
 
-const Model = () => {};
+  return {
+    domString,
+    render,
+    setGalleryTemplate,
+  };
+})();
 
-const Controller = () => {};
+const Model = ((view, api) => {
+  const fetchAlbums = api.getAllAlbums;
+
+  class State {
+    #allalbums = [];
+    #results = "";
+    // #artistname = "";
+
+    get results() {
+      return this.#results;
+    }
+    // get artistname() {
+    //   return this.#artistname;
+    // }
+
+    get allalbums() {
+      return this.#allalbums;
+    }
+    // set artistName(name) {
+    //   this.#artistname = name;
+    // }
+    set results(results) {
+      this.#results = results;
+      // const searchResult = document.getElementById(view.domString.searchResult);
+      // searchResult.innerHTML = `Find ${this.#results} albums of ${
+      //   this.#artistname
+      // }`;
+    }
+
+    set allalbums(albumlist) {
+      this.#allalbums = albumlist;
+      const albumGallery = document.getElementById(view.domString.albumGallery);
+      const template = view.setGalleryTemplate(this.#allalbums);
+      view.render(albumGallery, template);
+    }
+  }
+
+  return {
+    fetchAlbums,
+    State,
+  };
+})(View, fetchAPI);
+
+const Controller = ((model, view) => {
+  const state = new model.State();
+
+  const inputListener = () => {
+    const searchInputEle = document.getElementById(view.domString.searchInput);
+    searchInputEle.addEventListener("change", (event) => {
+      console.log("event listener works");
+      let name = event.target.value;
+      if (name !== 0) {
+        const searchTerm = name.toLowerCase().split(" ").join("+");
+        // console.log(state.artistname);
+        model.fetchAlbums(searchTerm).then((data) => {
+          state.allalbums = data.results;
+          state.results = data.resultCount;
+
+          const searchResult = document.getElementById(
+            view.domString.searchResult
+          );
+          searchResult.innerHTML = `Find ${state.results} albums of ${name}`;
+        });
+      }
+    });
+  };
+
+  const init = () => {
+    console.log("init");
+    inputListener();
+  };
+
+  return {
+    init,
+  };
+})(Model, View);
+
+Controller.init();
