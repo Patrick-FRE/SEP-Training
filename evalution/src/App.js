d518700443f716798a2264b75b67c5b5274c81f4
Alden <c2liang@bu.edu>
2021-02-15 14:36:11 -0500

    basic requirement 1

--- evalution/src/App.js
+++ evalution/src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <div className="App"></div>;
+  const searchName = 1;
+  const albums = [];
+  return (
+    <div className="App">
+      <header>
+        <input placeholder={`Search...`}></input>
+      </header>
+      <main>
+        <header>{`Search ALbums by ${
+          searchName ? searchName : "ArtisName"
+        }`}</header>
+        <section>
+          {albums.length > 0
+            ? albums.map((album) => {
+                <div>albums.name</div>;
+              })
+            : null}
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
