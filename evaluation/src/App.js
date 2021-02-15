d2b27aef85d7e2562fa24df7534bd497e76a7b91
JYY522 <772144452@qq.com>
2021-02-15 15:38:43 -0500

    set up redux

--- evaluation/src/App.js
+++ evaluation/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import{BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import  Nav  from './components/nav/nav.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hello
-      
-    </div>
-  );
-}
 
-export default App;
+function app(){
+  return(
+    <Router>
+    <div className = "App">
+    <Nav/>
+    {/* <Switch>
+    <Route path="/" exact component={HomePage}/>
+    <Route path="/:name"exact component = {userDetail}/>
+    <Route path="/:name/followers" component={Followers}/>
+    </Switch> */}
+    </div>
+    </Router>
+  )
+}
+export default app;
---
+++ evaluation/src/actions/searchActions.js
@@ -0,0 +1,14 @@
+import { SEARCH_MOVIE} from './types';
+
+
+
+
+export const searchMovie = text => dispatch => {
+  dispatch({
+    type: SEARCH_MOVIE,
+    payload: text
+  });
+};
+
+
+
