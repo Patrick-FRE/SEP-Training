2b950a55a48038d792294dbf3489741eae17e60f
Esther Qiu <qiu00057@umn.edu>
2021-02-15 22:39:00 -0600

    Optimi. after final eval: empty string fetch 0 results from API

--- evaluation/src/actions/albumAction.js
+++ evaluation/src/actions/albumAction.js
@@ -2,12 +2,12 @@ import { searchAlbum } from "../apis/itunes.api";
 export const SEARCH_ALBUMS = "SEARCH_ALBUMS";
 
 export const searchAlbums = (name) => (dispatch) => {
-  const param = name.split(" ").join("+");
+  const param = name ? name.split(" ").join("+") : null;
   searchAlbum(param).then((data) => {
     const results = data.results;
     dispatch({
       type: SEARCH_ALBUMS,
-      payload: { albums: results, searchedName: name },
+      payload: { albums: results.length > 1 ? results : [], searchedName: name },
     });
   });
 };
